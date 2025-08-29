ad7c27de00d78c3378dd8f1f6d8feb19c8d040ae
Kirti Sharma <kirtisharmaa8433@gmail.com>
2025-08-29 11:42:04 +0530

    29/August/25 Exception At RunTime

---
+++ src/Bank.java
@@ -0,0 +1,12 @@
+public class Bank {
+    private int balance=10000;
+    public void withdrawl(int amount){
+        if(amount>balance){
+            throw new InsufficientBalanceException("Cannot withdraw amount greater than ballance");
+        }
+        else{
+            balance=balance-amount;
+            System.out.println("Withdrawl Successful,Remaining Balance="+balance);
+        }
+    }
+}
