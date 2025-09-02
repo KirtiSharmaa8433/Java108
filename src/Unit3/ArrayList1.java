88a467301bf67ab4ed09811f49a22e215d318620
Kirti Sharma <kirtisharmaa8433@gmail.com>
2025-09-02 12:06:08 +0530

    01/September/25 Array List

---
+++ src/Unit3/ArrayList1.java
@@ -0,0 +1,15 @@
+package Unit3;
+import java.util.ArrayList;
+public class ArrayList1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> a1=new ArrayList<>();
+        a1.add(12);
+        a1.add(13);
+        a1.add(34);
+        a1.add(54);
+        System.out.println(a1);
+        for(int al1:a1){
+            System.out.println(al1);
+        }
+    }
+}
