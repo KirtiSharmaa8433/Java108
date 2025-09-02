0cc39756de623e812ac065b52de63cb7c69866f9
Kirti Sharma <kirtisharmaa8433@gmail.com>
2025-09-02 12:18:42 +0530

    01/September/25 Array List2

---
+++ src/Unit3/ArrayList2.java
@@ -0,0 +1,25 @@
+package Unit3;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.Collections;
+public class ArrayList2 {
+    public static void main(String[] args) {
+        ArrayList<String> products=new ArrayList<>();
+        System.out.println("Enter the number of products");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Enter products details");
+        for(int i=0;i<n;i++){
+            products.add(sc.next());
+        }
+
+        Collections.sort(products);
+        Iterator it=products.iterator();
+
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+    }
+}
