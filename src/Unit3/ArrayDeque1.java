fcb91fde1c37ecb0dedf1ae4da909cadf617e124
Kirti Sharma <kirtisharmaa8433@gmail.com>
2025-09-12 11:02:01 +0530

    11/September/25 Queue

---
+++ src/Unit3/ArrayDeque1.java
@@ -0,0 +1,15 @@
+package Unit3;
+
+import java.util.ArrayDeque;
+
+public class ArrayDeque1 {
+    public static void main(String[] args) {
+        ArrayDeque<String> ad=new ArrayDeque<>();
+        ad.add("First");
+        ad.addFirst("0");
+        ad.addLast("9");
+
+
+
+    }
+}
